8504a7f1e35c4a5f4a06addb972f2ec99eb0266b
wajima <miswndu@yahoo.co.jp>
2018-11-04 16:49:16 +0900

    パスワードの長さを８文字以上、半角英字、数字をそれぞれ１文字以上の条件を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :timeoutable, :lockable
+
+  validate :password_complexity
+  def password_complexity
+    return if password.blank? || password =~ /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,100}+\z/i
+    errors.add :password, "パスワードの強度が不足しています。パスワードの長さは8文字以上とし、小文字と数字と特殊文字をそれぞれ1文字以上含める必要があります。"
+  end
 end
