8a25a1b3e74e6664493e02a47954493c46c1bc24
wajima <miswndu@yahoo.co.jp>
2018-11-07 23:38:43 +0900

    feature specのエラーパターンを追加

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -163,7 +163,7 @@ Devise.setup do |config|
 
   # ==> Configuration for :validatable
   # Range for password length.
-  config.password_length = 6..128
+  config.password_length = 8..128
 
   # Email regex used to validate email formats. It simply asserts that
   # one (and only one) @ exists in the given string. This is mainly
@@ -193,7 +193,7 @@ Devise.setup do |config|
 
   # Number of authentication tries before locking an account if lock_strategy
   # is failed attempts.
-  config.maximum_attempts = 3
+  config.maximum_attempts = 4
 
   # Time interval to unlock the account if :time is enabled as unlock_strategy.
   config.unlock_in = 1.minute
--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -5,14 +5,64 @@ RSpec.feature "Users", type: :feature do
     user = FactoryBot.create(:user)
 
     visit new_user_session_path
-    fill_in "user[email]"                , with: user.email
-    fill_in "user[password]"             , with: user.password
+    fill_in "user[email]"   , with: user.email
+    fill_in "user[password]", with: user.password
     click_button "ログイン"
 
     expect(page).to have_content "ログインしました。"
   end
 
+  scenario "正常なユーザ登録" do
+    user = FactoryBot.build(:user)
 
+    visit new_user_registration_path
+    fill_in "user[username]"             , with: user.username
+    fill_in "user[email]"                , with: user.email
+    fill_in "user[password]"             , with: user.password
+    fill_in "user[password_confirmation]", with: user.password
+    click_button "アカウント登録"
+
+    expect(page).to have_content"アカウント登録が完了しました。"
+  end
+
+  scenario "パスワードを３回まで間違えてもアカウントロックされない" do
+    user = FactoryBot.create(:user)
+    3.times do
+      visit new_user_session_path
+      fill_in "user[email]"   , with: user.email
+      fill_in "user[password]", with: "hogehoge123"
+      click_button "ログイン"
+
+      expect(page).to have_content"Eメールまたはパスワードが違います。"
+      expect(current_path).to eq(new_user_session_path)
+    end
+
+    fill_in "user[email]"   , with: user.email
+    fill_in "user[password]", with: user.password
+    click_button "ログイン"
+
+    expect(page).to have_content "ログインしました。"
+  end
+
+  scenario "パスワードを4回間違えるとアカウントロックされる" do
+    user = FactoryBot.create(:user)
+    4.times do
+      visit new_user_session_path
+      fill_in "user[email]"   , with: user.email
+      fill_in "user[password]", with: "hogehoge123"
+      click_button "ログイン"
+
+      expect(page).to have_content"Eメールまたはパスワードが違います。"
+      expect(current_path).to eq(new_user_session_path)
+    end
+
+    fill_in "user[email]"   , with: user.email
+    fill_in "user[password]", with: user.password
+    click_button "ログイン"
+
+    expect(page).to have_content"Eメールまたはパスワードが違います。"
+    expect(current_path).to eq(new_user_session_path)
+  end
 end
 
 
