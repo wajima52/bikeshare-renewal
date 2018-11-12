46f4895bd6dfc4edeb8d1ae4f0abf8c960fe4303
wajima <miswndu@yahoo.co.jp>
2018-11-13 00:08:33 +0900

    Bicycle newアクション テスト含め実装終了

---
+++ spec/features/bicycles_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.feature "Bicycles", type: :feature do
+
+  before do
+    @user = FactoryBot.create(:user)
+
+    visit new_user_session_path
+    fill_in "user[email]"   , with: @user.email
+    fill_in "user[password]", with: @user.password
+    click_button "ログイン"
+  end
+
+  scenario "正常なBicycle投稿" do
+    visit new_bicycle_path
+    fill_in "bicycle[name]", with: "Fenix"
+    select "ロードバイク", from: "bicycle[bicycle_type]"
+    click_button "登録"
+
+    expect(page).to have_content("正常に登録されました")
+  end
+
+  scenario "名前を未入力で「登録」ボタンをクリック" do
+    visit new_bicycle_path
+    select "ロードバイク", from: "bicycle[bicycle_type]"
+    click_button "登録"
+
+    expect(page).to have_content("自転車の名前を入力してください")
+    expect(current_path).to eq('/bicycles')
+  end
+end
---
+++ spec/models/bicycle_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+
+RSpec.describe Bicycle, type: :model do
+
+  let(:bicycle) {FactoryBot.build(:bicycle)}
+
+  it "factoryが有効であること" do
+    bicycle.valid?
+
+    expect(bicycle).to be_valid
+  end
+
+  describe "バリデーションテスト" do
+
+    it "nameが空のときにエラー" do
+      bicycle.name = nil
+      bicycle.valid?
+
+      expect(bicycle).not_to be_valid
+    end
+
+    it "typeが空の時にエラー" do
+      bicycle.bicycle_type = nil
+      bicycle.valid?
+
+      expect(bicycle).not_to be_valid
+    end
+
+  end
+
+  describe "リレーションテスト" do
+    it "ユーザーに紐づかないBicycleはエラー" do
+      bicycle.user = nil
+      bicycle.valid?
+
+      expect(bicycle).not_to be_valid
+    end
+
+    it "ユーザが削除された時、紐づくBicycleも削除されること" do
+      bicycle.save
+      user = bicycle.user
+      expect{user.destroy}.to change{Bicycle.count}.by(-1)
+    end
+  end
+end
